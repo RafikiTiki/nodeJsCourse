7291ddeeaba69c4bbabe8a444cca9a996e35b283
RafikiTiki <wow1926@gmail.com>
2017-03-08 18:50:32 +0100

    connect mongodb with heroku

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const flash = require('connect-flash')
 const app = express()
 
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://heroku_7l863gl3:uicja6qlg6n0dmi4k4qh8ro3t2@ds121980.mlab.com:21980/heroku_7l863gl3')
+mongoose.connect('mongodb://Rafiki:Kotek1@ds121980.mlab.com:21980/heroku_7l863gl3')
 
 require('./config/passport')
 
