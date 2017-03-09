16b38f8b5e4a0140428f49fba621d4148d270bda
RafikiTiki <wow1926@gmail.com>
2017-03-09 01:36:16 +0100

    add reset password functionality

--- models/user.js
+++ models/user.js
@@ -1,27 +1,26 @@
-const mongoose = require('mongoose')
-const bcrypt = require('bcrypt-nodejs')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt-nodejs");
+const Schema = mongoose.Schema;
 
 const userSchema = Schema({
-  fullname: {type: String, required: true},
-  email: {type: String, required: true},
-  password: {type: String},
-  role: {type: String, default: ''},
+  fullname: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String },
+  role: { type: String, default: "" },
   company: {
-    name: {type: String, default: ''},
-    image: {type: String, default: ''},
+    name: { type: String, default: "" },
+    image: { type: String, default: "" }
   },
-  passwordResetToken: {type: String, default: ''},
-  passwordResetExpires: {type: Date, default: Date.now()}
-})
+  passwordResetToken: { type: String, default: "" },
+  passwordResetExpires: { type: Date, default: Date.now() }
+});
 
 userSchema.methods.encryptPassword = password => {
-  return bcrypt.hashSync(password, bcrypt.genSaltSync(10), null)
-}
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(10), null);
+};
 
 userSchema.methods.validPassword = function(password) {
-  return bcrypt.compareSync(password, this.password)
-}
+  return bcrypt.compareSync(password, this.password);
+};
 
-
-module.exports = mongoose.model("User", userSchema)
+module.exports = mongoose.model("User", userSchema);
--- server.js
+++ server.js
@@ -1,58 +1,57 @@
-const express = require('express')
-const coockieParser = require('cookie-parser')
-const bodyParser = require('body-parser')
-const validator = require('express-validator')
-const ejs = require('ejs')
-const engine = require('ejs-mate')
-const session = require('express-session')
-const mongoose = require('mongoose')
-const MongoStore = require('connect-mongo')(session)
-const passport = require('passport')
-const flash = require('connect-flash')
+const express = require("express");
+const coockieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const validator = require("express-validator");
+const ejs = require("ejs");
+const engine = require("ejs-mate");
+const session = require("express-session");
+const mongoose = require("mongoose");
+const MongoStore = require("connect-mongo")(session);
+const passport = require("passport");
+const flash = require("connect-flash");
 
+const app = express();
 
-const app = express()
+const uri = "mongodb://Rafiki:Kotek1@ds121980.mlab.com:21980/heroku_7l863gl3";
 
-const uri = 'mongodb://Rafiki:Kotek1@ds121980.mlab.com:21980/heroku_7l863gl3'
-
-console.log('MongoDB URI:', uri)
-
-mongoose.Promise = global.Promise
-mongoose.connect(uri, function (err, res) {
+mongoose.Promise = global.Promise;
+mongoose.connect(uri, function(err, res) {
   if (err) {
-    console.log ('ERROR connecting to: ' + uri + '. ' + err);
+    console.log("ERROR connecting to: " + uri + ". " + err);
   } else {
-    console.log ('Succeeded connected to: ' + uri);
+    console.log("Succeeded connected to: " + uri);
   }
-})
+});
 
-require('./config/passport')
+require("./config/passport");
 
-app.use(express.static('public'))
-app.engine('ejs', engine)
-app.set('view engine', 'ejs')
-app.use(coockieParser())
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
+app.use(express.static("public"));
+app.engine("ejs", engine);
+app.set("view engine", "ejs");
+app.use(coockieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.use(validator())
+app.use(validator());
 
-app.use(session({
-  secret: 'Thisismyteskey',
-  resave: false,
-  saveUninitialized: false,
-  store: new MongoStore({mongooseConnection: mongoose.connection})
-}))
+app.use(
+  session({
+    secret: "Thisismyteskey",
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
+  })
+);
 
-app.use(flash())
+app.use(flash());
 
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(passport.initialize());
+app.use(passport.session());
 
-require('./routes/user')(app, passport)
+require("./routes/user")(app, passport);
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log('App runing on ...', port)
-})
+  console.log("App runing on ...", port);
+});
